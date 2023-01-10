0c20e09721fb25719d68e891dd93a3c23ce58487
Matthias Arzt <arzt@mpi-cbg.de>
2023-01-10 13:13:29 +0100

    Add code to format the java code that changed between to versions of a file

--- pr_formatter/__init__.py
+++ pr_formatter/__init__.py
@@ -1,4 +1,10 @@
+import difflib
+import os
 import re
+import shutil
+import tempfile
+import subprocess
+import git_filter_repo as fr
 
 
 def write_file(content, tmpfile):
@@ -14,6 +20,69 @@ def read_file(tmpfile):
 def parse_range(text):
     regex = re.compile(r'^@@ -\d+(?:,\d+)? \+(\d+)(?:,(\d+))? @@')
     match = regex.search(text)
+    if match is None:
+        raise ValueError(
+            "The line looks like a mal formatter unified diff: " + text)
     start = int(match.group(1))
     count = int(match.group(2) or 1)
     return start, start + count
+
+
+def parse_ranges(diff):
+    return [parse_range(line) for line in diff if line[0] == '@']
+
+
+def insert_new_line_markers(old, new, begin, end):
+    diff = difflib.unified_diff(old, new, n=0)
+    ranges = parse_ranges(diff)
+    n = 0
+    lines = []
+    for (start_line, end_line) in ranges:
+        lines.extend(new[n: start_line - 1])
+        lines.append(begin)
+        lines.extend(new[start_line - 1: end_line - 1])
+        lines.append(end)
+        n = end_line - 1
+    lines.extend(new[n:])
+    return lines
+
+
+def insert_formatter_statements(old, new):
+    formatter_on = "// @formatter:on PULL-REQUEST-FORMATTER"
+    formatter_off = "// @formatter:off PULL-REQUEST-FORMATTER"
+    return [formatter_off] + insert_new_line_markers(
+        old, new, begin=formatter_on, end=formatter_off)
+
+
+def remove_formatter_statements(lines):
+    formatter_on = "// @formatter:on PULL-REQUEST-FORMATTER"
+    formatter_off = "// @formatter:off PULL-REQUEST-FORMATTER"
+    return [line for line in lines if line not in [formatter_on, formatter_off]]
+
+
+def format_java(pom, code):
+    temp_dir = tempfile.TemporaryDirectory()
+    shutil.copy(pom, temp_dir.name + "/pom.xml")
+    source_dir = temp_dir.name + "/src/main/java"
+    os.makedirs(source_dir, exist_ok=False)
+    java_file = source_dir + "/file.java"
+    write_file('\n'.join(code).encode('utf8'), java_file)
+    subprocess.check_call(["mvn", "formatter:format"], cwd=temp_dir.name)
+    formatted_code = read_file(java_file).decode('utf8').split('\n')
+    temp_dir.cleanup()
+    return formatted_code
+
+
+def format_changes(pom, a, b):
+    text = insert_formatter_statements(a, b)
+    text = format_java(pom, text)
+    text = remove_formatter_statements(text)
+    return text
+
+
+def format_changes_binary(pom, a, b):
+    lines = format_changes(pom,
+                           a.decode('utf8').split('\n'),
+                           b.decode('utf8').split('\n'))
+    return '\n'.join(lines).encode('utf8')
+
--- test_sample.py
+++ test_sample.py
@@ -1,5 +1,6 @@
 import pr_formatter
 import difflib
+import textwrap
 
 
 def test_add():
@@ -21,4 +22,110 @@ def test_unified_diff():
 def test_parse_range():
     assert pr_formatter.parse_range('@@ -2 +2 @@') == (2, 3)
     assert pr_formatter.parse_range('@@ -25,23 +45,10 @@ something') == (45, 55)
+
+
+def test_parse_ranges():
+    diff = ["--- something",
+            "+++ else",
+            "@@ -2 +2 @@ foo",
+            "- hello",
+            "+ world",
+            "@@ -17 +17,2 @@",
+            "- foo bar",
+            "+ foo",
+            "+ bar"]
+    ranges = pr_formatter.parse_ranges(diff)
+    assert ranges == [(2, 3), (17, 19)]
+
+
+def test_insert_new_line_markers():
+    a = ["a", "b", "c"]
+    b = ["a", "bb", "c"]
+    r = pr_formatter.insert_new_line_markers(
+        a, b, begin="_begin_", end="_end_")
+    assert r == ["a", "_begin_", "bb", "_end_", "c"]
+
+
+def test_insert_formatter_statements():
+    a = textwrap.dedent("""\
+        class Example {
+            boolean getBoolean() {
+                return false;
+            }
+        }
+        """).split('\n')
+    b = [line.replace("false", "true") for line in a]
+    expected = textwrap.dedent("""\
+        // @formatter:off PULL-REQUEST-FORMATTER
+        class Example {
+            boolean getBoolean() {
+        // @formatter:on PULL-REQUEST-FORMATTER
+                return true;
+        // @formatter:off PULL-REQUEST-FORMATTER
+            }
+        }
+        """).split('\n')
+    assert pr_formatter.insert_formatter_statements(old=a, new=b) == expected
+
+
+def test_remove_formatter_statements():
+    text = textwrap.dedent("""\
+        a
+        // @formatter:on PULL-REQUEST-FORMATTER
+        b
+        // @formatter:off PULL-REQUEST-FORMATTER
+        c
+        """).split('\n')
+    expected = ["a", "b", "c", ""]
+    assert expected == pr_formatter.remove_formatter_statements(text)
+
+
+def test_format_java():
+    text = textwrap.dedent("""\
+        class Example {
+            boolean getBoolean() {
+                return     true;
+            }
+        }
+        """).split('\n')
+    pom = "resources/example_pom.xml"
+    expected = textwrap.dedent("""\
+        class Example {
+        
+        \tboolean getBoolean() {
+        \t\treturn true;
+        \t}
+        }
+        """).split('\n')
+    assert expected == pr_formatter.format_java(pom, text)
+
+
+def test_format_changes():
+    pom = "resources/example_pom.xml"
+    a = textwrap.dedent("""\
+        class Example {
+        
+        \tboolean    getBoolean() {
+        \t\treturn    false;
+        \t}
+        }
+        """).split('\n')
+    b = [line.replace("false", "true") for line in a]
+    expected = [line.replace("   false", "true") for line in a]
+    assert expected == pr_formatter.format_changes(pom, a, b)
+
+def test_format_changes_binary():
+    pom = "resources/example_pom.xml"
+    a = textwrap.dedent("""\
+        class Example {
+        
+        \tboolean    getBoolean() {
+        \t\treturn    false;
+        \t}
+        }
+        """)
+    bytes_a = a.encode('utf8')
+    bytes_b = a.replace('false', 'true').encode('utf8')
+    expected = a.replace("   false", "true").encode('utf8')
+    assert expected == pr_formatter.format_changes_binary(pom, bytes_a, bytes_b)
 
